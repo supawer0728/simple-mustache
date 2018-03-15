eb132d815ed933e7f1e5cfab4d4cef679d9a238c
Jihoon.Jeon <jihoon.jeon@nhnent.com>
2018-03-15 17:33:51 +0900

    불필요한 주석 제거

--- src/main/java/com/parfait/study/simplemustache/SimpleMustacheApplication.java
+++ src/main/java/com/parfait/study/simplemustache/SimpleMustacheApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SimpleMustacheApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SimpleMustacheApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleMustacheApplication.class, args);
+    }
 }
